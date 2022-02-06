259ff248e768d11c4de1e7d3357ea5d5d5351f37
Vidar Eldøy <vidar@eldoy.com>
2022-02-06 19:02:54 +0100

    Move web socket to file

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const rekvest = require('rekvest')
 const setup = require('./lib/setup.js')
 const run = require('./lib/run.js')
 const server = require('./lib/server.js')
+const socket = require('./lib/socket.js')
 
 const log = require('./lib/log.js')
 
@@ -38,6 +39,10 @@ module.exports = function(config = {}) {
 
   // Set up web server
   const http = server(state, config)
+
+  // Set up web socket
+  const websocket = socket(http, state, config, publish)
+
 
   /***************************************************************
   /* PUBSUB
@@ -94,74 +99,6 @@ module.exports = function(config = {}) {
     if (callback) callback()
   }
 
-
-  /***************************************************************
-  * WEBSOCKET
-  ***************************************************************/
-
-  // Set up web socket
-  const websocket = new ws.Server({ server: http })
-
-  // The websocket callback
-  async function websocketRequest(client, req) {
-    rekvest(req)
-
-    // Additional properties
-    client.req = req
-    client.isAlive = true
-    client.id = uuid()
-
-    // Support promises and JSON for client send
-    client.deliver = client.send
-
-    client.send = function(data, options = {}, fn) {
-      if (typeof options == 'function') {
-        fn = options
-        options = {}
-      }
-      if (typeof data == 'object') {
-        data = JSON.stringify(data)
-      }
-      return new Promise(resolve => {
-        client.deliver(data, options, () => fn ? fn() : resolve())
-      })
-    }
-
-    client.publish = function(name, data, options, fn) {
-      return publish(name, data, options, fn, client)
-    }
-
-    // Run websocket connect callback
-    if (typeof config.connect == 'function') {
-      await config.connect(client)
-    }
-
-    client.on('pong', () => client.isAlive = true)
-    client.on('close', () => client.isAlive = false)
-
-    async function webSocketMessage(data) {
-      // Extract data and action
-      data = JSON.parse(data)
-      const name = data.$action || '*'
-      const action = state.actions[name]
-      delete data.$action
-
-      // Log request
-      log(`WS /${name}`, data)
-
-      // Run action and send result
-      if (action) {
-        const result = await run(action, state.handlers.fail, data, client)
-        if (typeof result != 'undefined') {
-          client.send(result)
-        }
-      }
-    }
-    client.on('message', webSocketMessage)
-  }
-
-  websocket.on('connection', websocketRequest)
-
   // Publish to pubsub channel
   function publish(name, data, options = {}, fn, client) {
     if (typeof options == 'function') {
@@ -185,19 +122,6 @@ module.exports = function(config = {}) {
       }
     })
   }
-
-  // Terminate stale clients
-  function terminateStaleClients() {
-    websocket.clients.forEach(client => {
-      if (client.isAlive === false) {
-        return client.terminate()
-      }
-      client.isAlive = false
-      client.ping(function(){})
-    })
-  }
-
-  setInterval(terminateStaleClients, 30000)
 
 
   /***************************************************************
