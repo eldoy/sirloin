59fee6923619ccba2fffd384a3f248c7e9023b3e
Vidar Eldøy <vidar@eldoy.com>
2022-02-06 18:13:19 +0100

    Move HTTP to file

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const util = require('util')
-const protocols = {
-  http: require('http'),
-  https: require('https')
-}
-const bodyParser = require('bparse')
-const serveStatic = require('hangersteak')
-const cookie = require('wcookie')
-
 const Redis = require('ioredis')
 
 const { v4: uuid } = require('uuid')
@@ -15,14 +6,12 @@ const rekvest = require('rekvest')
 
 const configure = require('./lib/configure.js')
 const run = require('./lib/run.js')
+const server = require('./lib/server.js')
+
+const log = require('./lib/log.js')
 
 const METHODS = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
 
-function log(msg, data = {}) {
-  if (process.env.NODE_ENV != 'production') {
-    console.log(`\n${msg}\n${util.inspect(data)}`)
-  }
-}
 
 module.exports = function(config = {}) {
 
@@ -47,71 +36,8 @@ module.exports = function(config = {}) {
   // Add defaults and set up config
   configure(config)
 
-
-  /***************************************************************
-  /* HTTP
-  ***************************************************************/
-
-  // Serve data requests
-  function serveData(req, res, data) {
-    switch (typeof data) {
-      case 'undefined': res.statusCode = 404
-      case 'object': data = JSON.stringify(data || '')
-      default: data = String(data)
-    }
-    if (req.cookieJar && req.cookieJar.length) {
-      res.setHeader('set-cookie', req.cookieJar.headers)
-    }
-    res.setHeader('content-length', Buffer.byteLength(data))
-    res.end(data)
-  }
-
-  // The http request callback
-  async function httpRequest(req, res) {
-    rekvest(req)
-    res.setHeader('Content-Type', 'application/json; charset=utf-8')
-    cookie(req)
-    let data, isStaticRequest = ['GET', 'HEAD'].includes(req.method)
-
-    // Log request
-    if (!isStaticRequest) {
-      log(`HTTP ${req.pathname}`, req.params)
-    }
-
-    // Run middleware
-    for (const mw of state.middleware) {
-      data = await run(mw, state.handlers.error, req, res)
-      if (typeof data != 'undefined') break
-    }
-
-    // Process route
-    if (typeof data == 'undefined') {
-      if (req.method == 'OPTIONS') data = ''
-      const map = state.routes[req.method]
-      if (map) {
-        const route = map[req.pathname] || map['*']
-        if (route) {
-          if (req.method == 'POST') await bodyParser(req)
-          data = await run(route, state.handlers.error, req, res)
-        }
-      }
-    }
-
-    // Serve static if still no match
-    if (typeof config.dir == 'string' && typeof data == 'undefined' && isStaticRequest) {
-      serveStatic(req, res, { dir: config.dir })
-    } else {
-      serveData(req, res, data)
-    }
-  }
-
-  // Create HTTP server
-  const client = protocols[config.ssl ? 'https' : 'http']
-  const http = client.createServer(config.ssl, httpRequest)
-
-  // Listen to port
-  http.listen(config.port)
-  console.log('Web server is listening on port %d', config.port)
+  // Set up web server
+  const http = server(state, config)
 
 
   /***************************************************************
