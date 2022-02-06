7082aa349ea40f391d9656dc63ac7503b7974eff
Vidar Eldøy <vidar@eldoy.com>
2022-02-06 19:30:31 +0100

    Move api to file

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 const { v4: uuid } = require('uuid')
 const ws = require('ws')
 const rekvest = require('rekvest')
-
 const setup = require('./lib/setup.js')
 const run = require('./lib/run.js')
 const server = require('./lib/server.js')
 const socket = require('./lib/socket.js')
 const sub = require('./lib/sub.js')
-
+const core = require('./lib/core.js')
 const log = require('./lib/log.js')
 
-const METHODS = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
-
-
 module.exports = function(config = {}) {
-
-  // Public api functions and properties
-  const api = {}
 
   // Internal state
   const state = {
@@ -40,8 +33,7 @@ module.exports = function(config = {}) {
   const http = server(state, config)
 
   // Set up web socket
-  // TODO: Move publish earlier, or move setup here
-  const websocket = socket(http, state, config, publish)
+  const websocket = socket(http, publish, state, config)
 
   // Set up pub sub
   const pubsub = sub(websocket, state, config)
@@ -70,61 +62,13 @@ module.exports = function(config = {}) {
     })
   }
 
-
-  /***************************************************************
-  /* API
-  ***************************************************************/
-
-  // Generate verb functions
-  for (const m of METHODS) {
-    state.routes[m] = {}
-    api[m.toLowerCase()] = function(path, fn) {
-      state.routes[m][path] = fn
-    }
-  }
-
-  // Match specific methods
-  api.all = function(path, fn, methods = METHODS) {
-    for (const m of methods) {
-      api[m.toLowerCase()](path, fn)
-    }
-  }
-
-  // Match any method
-  api.any = function(...args) {
-    const [fn, path] = args.reverse()
-    api.all(path, fn)
-  }
-
-  // Use middleware
-  api.use = function(fn) {
-    state.middleware.push(fn)
-  }
-
-  // Match action name
-  api.action = function(name, fn) {
-    state.actions[name] = fn
-  }
-
-  // Subscribe to publish
-  api.subscribe = function(name, fn) {
-    state.handlers[name] = fn
-  }
-
-  // HTTP error
-  api.error = function(fn) {
-    state.handlers.error = fn
-  }
-
-  // Websocket fail
-  api.fail = function(fn) {
-    state.handlers.fail = fn
-  }
-
   // Public functions and properties
+  const api = core(state)
+
   api.http = http
   api.websocket = websocket
   api.publish = publish
+  api.pubsub = pubsub
   api.config = config
 
   return api
